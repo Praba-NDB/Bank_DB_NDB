8e65daa6e53ba14df20174cfb4238e45764a96ad
Praba-NDB <prabakaran.c@nilandatabytes.com>
2026-09-23 23:06:53 -0700

    Create TestQueries.sql

---
+++ TestQueries.sql
@@ -0,0 +1,17 @@
+SELECT
+    CustomerName,
+    AccountNumber,
+    Balance
+FROM dbo.BankDetails
+WHERE Balance > 50000
+ORDER BY Balance DESC;
+
+SELECT *
+FROM dbo.BankDetails
+WHERE AccountStatus = 'Active';
+
+
+
+SELECT *
+FROM dbo.BankDetails
+WHERE AccountType = 'Savings';
